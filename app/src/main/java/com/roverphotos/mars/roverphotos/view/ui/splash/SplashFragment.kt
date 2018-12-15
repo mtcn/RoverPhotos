6da1c0cecc83eec56a3335354ccbbde6a921d885
Metecan Duyal <metecanduyal@gmail.com>
2018-12-16 00:59:15 +0300

    splash fragment animation & roverdata request added

--- app/src/main/java/com/roverphotos/mars/roverphotos/view/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/roverphotos/mars/roverphotos/view/ui/splash/SplashFragment.kt
@@ -1,18 +1,24 @@
 package com.roverphotos.mars.roverphotos.view.ui.splash
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.constraint.motion.MotionLayout
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.roverphotos.mars.roverphotos.R
 import com.roverphotos.mars.roverphotos.viewmodel.SplashViewModel
+import kotlinx.android.synthetic.main.splash_fragment.*
 
-class SplashFragment : Fragment() {
+class SplashFragment : Fragment(), MotionLayout.TransitionListener {
 
     companion object {
+        const val EXTRA_ROVER_DATA = "roverData"
         fun newInstance() = SplashFragment()
     }
 
@@ -28,7 +34,43 @@ class SplashFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.isComplete.observe(this, Observer { data ->
+            if (data == null)
+                return@Observer
+
+            val roverData = data.getOrElse(true) {
+                null
+            }
+
+            if (roverData != null) {
+                val bundle = Bundle()
+                bundle.putSerializable(EXTRA_ROVER_DATA, roverData)
+                Navigation.findNavController(
+                    activity!!,
+                    R.id.motionLayout
+                ).navigate(SplashFragmentDirections.actionSplashFragmentToChooseRoverFragment().actionId, bundle)
+            } else
+                Snackbar
+                    .make(motionLayout, "onError :", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Re-try") {
+                        viewModel.getRoversData()
+                    }
+                    .show()
+        })
+        startAnimation()
+    }
+
+    private fun startAnimation() {
+        motionLayout.setTransitionListener(this)
+        motionLayout.transitionToEnd()
+    }
+
+    override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+        viewModel.animationSubject.onNext(true)
+    }
+
+    override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+        //to do nothing
     }
 
 }
--- app/src/main/java/com/roverphotos/mars/roverphotos/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/roverphotos/mars/roverphotos/viewmodel/SplashViewModel.kt
@@ -1,7 +1,63 @@
 package com.roverphotos.mars.roverphotos.viewmodel
 
-import android.arch.lifecycle.ViewModel;
+import android.annotation.SuppressLint
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import android.util.Log
+import com.roverphotos.mars.roverphotos.api.NasaApi
+import com.roverphotos.mars.roverphotos.data.Rover
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SplashViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val responseSubject: BehaviorSubject<Boolean> by lazy { BehaviorSubject.create<Boolean>() }
+    val animationSubject: BehaviorSubject<Boolean> by lazy { BehaviorSubject.create<Boolean>() }
+    var isComplete = MutableLiveData<Map<Boolean, ArrayList<Rover>?>>()
+    var roverData = arrayListOf<Rover>()
+
+    init {
+        getRoversData()
+        observeEvents()
+    }
+
+    fun getRoversData() {
+        val service = NasaApi.create()
+        GlobalScope.launch {
+            val response = try {
+                service.getRovers().await()
+            } catch (e: Exception) {
+                null
+            }
+
+            if (response == null)
+                responseSubject.onNext(false)
+            else if (!response.rovers.isEmpty()) {
+                roverData.addAll(response.rovers)
+                responseSubject.onNext(true)
+            }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun observeEvents() {
+        val completionObservable: Observable<Boolean> = Observable.combineLatest(
+            animationSubject,
+            responseSubject,
+            BiFunction { a, r -> a && r })
+
+        completionObservable
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({ isDone ->
+                val map = HashMap<Boolean, ArrayList<Rover>?>()
+                map[isDone] = roverData
+                isComplete.postValue(map)
+            }, { err -> Log.e("ERROR", err.message) })
+    }
 }
